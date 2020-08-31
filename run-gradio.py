e8b0ddb79fdf3a74c599a66387fa86fe0aa41134
Abubakar Abid <a12d@stanford.edu>
2020-08-31 13:00:45 -0500

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -1,23 +1,18 @@
 import tensorflow as tf
+import numpy as np
+from urllib.request import urlretrieve
 import gradio as gr
-from gradio.outputs import Label
 
+urlretrieve("https://gr-models.s3-us-west-2.amazonaws.com/mnist-model.h5", "mnist-model.h5")
+model = tf.keras.models.load_model("mnist-model.h5")
 
-mnist_model = tf.keras.models.load_model('models/mnist-99.0-acc.h5')
+def recognize_digit(image):
+    image = image.reshape(1, -1)  # add a batch dimension
+    prediction = model.predict(image).tolist()[0]
+    return {str(i): prediction[i] for i in range(10)}
 
-def predict(inp):
-	prediction = mnist_model.predict(inp.reshape(1, 28, 28, 1)).tolist()[0]
-	return {str(i): prediction[i] for i in range(10)}
+output_component = gr.outputs.Label(num_top_classes=3)
 
-
-label = Label(num_top_classes=4)
-
-gr.Interface(
-	predict, 
-	"sketchpad", 
-	label,
-	capture_session=True,
-	title="MNIST Sketchpad",
-	description="Draw a number 0 through 9 on the sketchpad, and click submit to see the model's predictions. Model trained on the MNIST dataset.",
-	thumbnail="https://raw.githubusercontent.com/gradio-app/real-time-mnist/master/thumbnail2.png",
-	live=False,).launch();
+gr.Interface(fn=recognize_digit, 
+             inputs="sketchpad", 
+             outputs=output_component).launch();
